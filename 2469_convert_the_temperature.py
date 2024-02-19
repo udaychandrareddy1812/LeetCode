b6e6b1134d90a7df3a1295042fe209a16f289ce1
Uday Chandra Reddy <160356568+udaychandrareddy1812@users.noreply.github.com>
2024-02-19 08:40:39 -0600

    Update 2469_convert_the_temperature.py

--- 2469_convert_the_temperature.py
+++ 2469_convert_the_temperature.py
@@ -1,5 +1,5 @@
 class Solution:
-    def convert(self, celsius):
+    def convertTemperature(self, celsius: float) -> List[float]:
         kelvin = celsius + 273.15
         fahrenheit = (celsius * 1.80) + 32.00
         return [kelvin, fahrenheit]
